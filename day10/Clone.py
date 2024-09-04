83616f99c68bc488bd06102eb0dae881f5eeda52
baskarakash <baskarakash>
2024-09-04 07:07:28 +0000

    Added_Clonefile

---
+++ day10/Clone.py
@@ -0,0 +1,13 @@
+# Databricks notebook source
+# MAGIC %sql
+# MAGIC create table ab_upgrad_630069716375523.bronze.product_bronze_cloned
+# MAGIC deep clone ab_upgrad_630069716375523.bronze.products_bronze
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from ab_upgrad_630069716375523.bronze.products_bronze
+
+# COMMAND ----------
+
+
